10d2dd14afbf2c7e7ded9cbee57d38c26805907f
Matias Fuster <matias.fuster@solcre.com>
2015-01-05 15:51:26 -0200

    - PHPUnit configuration

---
+++ module/Columnis/test/ColumnisTest/Controller/PageControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ColumnisTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class PageControllerTest extends AbstractHttpControllerTestCase {
+
+    protected $traceError = true;
+
+    public function setUp() {
+        $this->setApplicationConfig(
+                include 'config/application.config.php'
+        );
+        parent::setUp();
+    }
+
+    public function testPageActionCanBeAccessed() {
+        /* Let's create a random route that must match the format "/lang/seo_friendly_name-pageId" */
+        $langs = array('espanol', 'english', 'portugues');
+        $lang = $langs[array_rand($langs)];
+        $randNameAr = array_merge(range('a','z'),range(0,9),array('-','_'));
+        shuffle($randNameAr);
+        $seoFriendlyName = implode('', array_splice($randNameAr, rand(0,(count($randNameAr)-1))));        
+        $pageId = rand(0,1000);        
+        $route = '/' . $lang . '/' . $seoFriendlyName . '-' . $pageId;
+        
+        $this->dispatch($route);
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('Columnis');
+        $this->assertControllerName('Columnis\Controller\Page');
+        $this->assertControllerClass('PageController');
+        $this->assertMatchedRouteName('columnis');
+    }
+
+}
---
+++ test/Bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+include_once(dirname(__FILE__) . '/../module/Columnis/test/Bootstrap.php');
