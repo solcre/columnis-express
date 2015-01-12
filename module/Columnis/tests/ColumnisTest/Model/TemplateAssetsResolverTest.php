f971a28382e9a368d0514b28f01a585bb2728040
vagrant <juan.barla@solcre.com>
2015-01-12 19:05:42 +0000

    all test except resolve

--- module/Columnis/tests/ColumnisTest/Model/TemplateAssetsResolverTest.php
+++ module/Columnis/tests/ColumnisTest/Model/TemplateAssetsResolverTest.php
@@ -6,15 +6,16 @@ use PHPUnit_Framework_TestCase;
 use Columnis\Model\TemplateAssetsResolver;
 
 class TemplateAssetsResolverTest extends PHPUnit_Framework_TestCase {
+
     protected $config;
-    
+
     public function setUp() {
         $configPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config/module.config.php';
         $this->config = include($configPath);
     }
 
     public function testTemplateExists() {
-        
+
         $assetsPaths = $this->config['asset_manager']['resolver_configs']['paths'];
         $templatesPathStack = $this->config['view_manager']['template_path_stack'];
 
@@ -22,9 +23,9 @@ class TemplateAssetsResolverTest extends PHPUnit_Framework_TestCase {
 
         $templateAssetsResolver = new TemplateAssetsResolver($assetsPaths, $templatesPathStack);
 
-        $randNameAr = array_merge(range('a','z'),range(0,9),array('-','_'));
+        $randNameAr = array_merge(range('a', 'z'), range(0, 9), array('-', '_'));
         shuffle($randNameAr);
-        $templateName = implode('', array_splice($randNameAr, rand(0,(count($randNameAr)-1))));
+        $templateName = implode('', array_splice($randNameAr, rand(0, (count($randNameAr) - 1))));
 
         $this->assertEquals(is_readable($arrayPaths), true);
 
@@ -37,51 +38,71 @@ class TemplateAssetsResolverTest extends PHPUnit_Framework_TestCase {
         if ($res) {
             $exists = $templateAssetsResolver->templateExists($templateName);
             $this->assertEquals(true, $exists);
-            
+
             rmdir($templatePath);
         }
     }
 
     public function testIs_subpath() {
-        $assetsPaths = array();
-        $templatesPathStack = array("./");
+        $templateAssetsResolver = $this->getTemplateAssetsResolver();
         $path = "folder";
         $subpath = "folder/other";
 
-        $templateAssetsResolver = new TemplateAssetsResolver($assetsPaths, $templatesPathStack);
         $res = $templateAssetsResolver->is_subpath($path, $subpath);
         $this->assertEquals($res, true);
     }
 
     public function testInAllowedPaths() {
-        $assetsPaths = array();
-        $templatesPathStack = array("./");
-        //el array allowed paths solo tiene './'
-        $name = "./";
+        $templateAssetsResolver = $this->getTemplateAssetsResolver();
+        $names = array(
+            "/var/www/columnisexpress/module/Columnis/config/../../../public/templates",
+            "/var/www/columnisexpress/module/Columnis/config/../../../public/css",
+            "/var/www/columnisexpress/module/Columnis/config/../../../public/js",);
 
-        $templateAssetsResolver = new TemplateAssetsResolver($assetsPaths, $templatesPathStack);
-        $res = $templateAssetsResolver->inAllowedPaths($name);
-        $this->assertEquals($res, true);
+        foreach ($names as $name) {
+            $res = $templateAssetsResolver->inAllowedPaths($name);
+
+            $this->assertEquals($res, true);
+        }
     }
-    
-    public function testGetTemplatePaths(){
-        $assetsPaths = array();
-        $templatesPathStack = array("./");
+
+    public function testGetTemplatePaths() {
+        $templateAssetsResolver = $this->getTemplateAssetsResolver();
         //el array allowed paths solo tiene './'
         $template = "error.phtml";
 
-        $templateAssetsResolver = new TemplateAssetsResolver($assetsPaths, $templatesPathStack);
-        
         $t = count($templateAssetsResolver->getTemplatePaths($template));
-        
-        $this->assertEquals($t,true);
+
+        $this->assertEquals($t, true);
     }
-    
-    private function dbg($p,$die=false){
+
+    public function testResolve() {
+
+        $templateAssetsResolver = $this->getTemplateAssetsResolver();
+
+        $name = "a";
+
+        $res = $templateAssetsResolver->resolve($name);
+
+        //$this->dbg($res,true);
+        //$this->assertEquals($res,true);
+    }
+
+    public function getTemplateAssetsResolver() {
+        $assetsPaths = $this->config['asset_manager']['resolver_configs']['paths'];
+        $templatesPathStack = $this->config['view_manager']['template_path_stack'];
+
+        $arrayPaths = $templatesPathStack[array_rand($templatesPathStack)];
+
+        return new TemplateAssetsResolver($assetsPaths, $templatesPathStack);
+    }
+
+    private function dbg($p, $die = false) {
         echo"<pre>";
         print_r($p);
-        if($die){
+        if ($die) {
             die();
         }
     }
+
 }
