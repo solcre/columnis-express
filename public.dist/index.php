8a66f55f3ef8d4be1cd662629d2110ba243e032f
Gustavo Rodriguez <gustavo.rodriguez@solcre.com>
2015-03-23 19:16:40 -0300

    Add production support for htaccess and index.php

--- public.dist/index.php
+++ public.dist/index.php
@@ -2,6 +2,8 @@
 /**
  * Not permanent fix to apache headers rename
  */
+$env = getenv('APP_ENV') ? : getenv('REDIRECT_APP_ENV') ? : 'production';
+
 if(isset($_SERVER["REDIRECT_HTTP_AUTHORIZATION"])){
     $_SERVER["HTTP_AUTHORIZATION"] = $_SERVER["REDIRECT_HTTP_AUTHORIZATION"];
 }
@@ -9,8 +11,12 @@ if(isset($_SERVER["REDIRECT_HTTP_AUTHORIZATION"])){
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-chdir(dirname(__DIR__));
+$appDir = '';
+if($env == 'production'){
+    $appDir = '/columnisexpress/current';
+}
 
+chdir(dirname(__DIR__).$appDir);
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
@@ -20,7 +26,7 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
 require 'init_autoloader.php';
 
 if (!defined('APPLICATION_PATH')) {
-    define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
+    define('APPLICATION_PATH', realpath(__DIR__ . '/..'.$appDir.'/'));
 }
 $appConfig = include APPLICATION_PATH . '/config/application.config.php';
 if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
