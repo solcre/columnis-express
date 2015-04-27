83373e0b1089969087214a3a8a027241b48fe034
Gustavo Rodriguez <gustavo.rodriguez@solcre.com>
2015-04-27 11:59:21 -0300

    -Now express use CORS (chrome-extension fix).

--- module/Columnis/Module.php
+++ module/Columnis/Module.php
@@ -1,34 +1,36 @@
 <?php
+
 namespace Columnis;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\Uri\UriFactory;
 
-class Module
-{
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
+class Module {
+
+    public function onBootstrap(MvcEvent $e) {
+        UriFactory::registerScheme('chrome-extension', 'Zend\Uri\Uri');
+
+        $eventManager = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
-         
+
         // get the cache listener service
         $cacheListener = $e->getApplication()->getServiceManager()->get('Columnis\Model\CacheListener');
 
         // attach the listeners to the event manager
         $e->getApplication()->getEventManager()->attach($cacheListener);
     }
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
+
+    public function getConfig() {
+        return include __DIR__.'/config/module.config.php';
     }
 
-    public function getAutoloaderConfig()
-    {
+    public function getAutoloaderConfig() {
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__.'/src/'.__NAMESPACE__,
                 ),
             ),
         );
--- module/ExpressApi/Module.php
+++ module/ExpressApi/Module.php
@@ -1,2 +1,4 @@
 <?php
+use Zend\Uri\UriFactory;
 require __DIR__ . '/src/ExpressApi/Module.php';
+UriFactory::registerScheme('chrome-extension', 'Zend\Uri\Uri');
