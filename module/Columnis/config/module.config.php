32016e7ee6d5ddb1252aa7bc96023a53a2081eef
Gustavo Rodriguez <gustavo.rodriguez@solcre.com>
2015-07-09 18:46:40 -0300

    -Set 404 and generic error page

--- module/Columnis/config/module.config.php
+++ module/Columnis/config/module.config.php
@@ -77,8 +77,8 @@ return array(
         'exception_template' => 'error/index',
         'template_map' => array(
             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.tpl',
+            'error/index' => __DIR__ . '/../view/error/index.tpl',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../../../public/templates',
