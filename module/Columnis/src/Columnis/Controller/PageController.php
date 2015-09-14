fd497d50a93fcdcc2eb7b1b6a0841d9d9fe8e23e
Gustavo Rodriguez <gustavo.rodriguez@solcre.com>
2015-09-14 15:06:09 -0300

    Merge page assets in the page response for easy manipulate and imports for development environment.

--- module/Columnis/src/Columnis/Controller/PageController.php
+++ module/Columnis/src/Columnis/Controller/PageController.php
@@ -5,6 +5,8 @@ namespace Columnis\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Columnis\Model\Page;
+use Columnis\Model\Template;
+use Columnis\Utils\Directory;
 use Columnis\Exception\Page\PageWithoutTemplateException;
 
 class PageController extends AbstractActionController {
@@ -23,6 +25,8 @@ class PageController extends AbstractActionController {
             $viewVariables = $page->getData();
             $template = $page->getTemplate();
             if($template->isValid()) {
+                $this->setPageAssets($template, $viewVariables);
+
                 $view = new ViewModel();
                 $view->setTemplate($template->getMainFile(false));
                 $view->setVariables($viewVariables);
@@ -53,5 +57,52 @@ class PageController extends AbstractActionController {
         /* @var $pageService \Columnis\Service\PageService */
         $pageService->setPageBreakpointService($withBreakpoints ? $serviceManager->get('Columnis\Service\PageBreakpointService') : null);
         return $pageService;
+    }
+
+    private function setPageAssets(Template $template, &$pageData) {
+        $jsAssets = $template->getAssets('js');
+        $cssAssets = $template->getAssets('css');
+        $paths = $this->getAssetsPath();
+
+        if(count($paths)) {
+            foreach($paths as $path) {
+                if(strpos($path, 'css') > -1) {
+                    $cssAssets = array_merge($cssAssets, $this->searchAssets($path, 'css'));
+                    sort($cssAssets);
+                }
+                else if(strpos($path, 'js') > -1) {
+                    $jsAssets = array_merge($jsAssets, $this->searchAssets($path, 'js'));
+                    sort($jsAssets);
+                }
+            }
+        }
+        if(is_array($pageData) && $pageData['pagina']) {
+            $pageData['pagina']['scripts'] = $jsAssets;
+            $pageData['pagina']['stylesheets'] = $cssAssets;
+        }
+        return '';
+    }
+
+    private function getAssetsPath() {
+        $config = $this->getServiceLocator()->get('Config');
+        if(is_array($config) &&
+                isset($config['asset_manager']) &&
+                is_array($config['asset_manager']['resolver_configs']) &&
+                isset($config['asset_manager']['resolver_configs']['paths'])) {
+            return $config['asset_manager']['resolver_configs']['paths'];
+        }
+        return array();
+    }
+
+    private function searchAssets($path, $extension) {
+        $assetPath = realpath($path.DIRECTORY_SEPARATOR.'fixed');
+        if(is_dir($assetPath)) {
+            $assetsPaths = Directory::recursiveSearchByExtension($assetPath, $extension);
+            $assets = array();
+            foreach($assetsPaths as $asset) {
+                $assets[] = str_replace($assetPath, $extension.DIRECTORY_SEPARATOR.'fixed', $asset);
+            }
+        }
+        return $assets;
     }
 }
