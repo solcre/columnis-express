959503a3faf0377de3b0aeb5d778512a3abf7ffe
Diego Sorribas <diego.sorribas@solcre.com>
2018-02-26 11:25:00 -0300

    fix(routes): delete unused routes

--- config/routes.php
+++ config/routes.php
@@ -28,10 +28,6 @@
 
 /** @var \Zend\Expressive\Application $app */
 
-//$app->get('/', App\Action\HomePageAction::class, 'home');
-$app->get('/api/ping', App\Action\PingAction::class, 'api.ping');
-
-
 /**
  * Templates Routes
  */
