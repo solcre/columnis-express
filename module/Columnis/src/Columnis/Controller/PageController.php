08bb42194d3b24a2c042ab67802ac6b20f993a89
Diego Sorribas <diego.sorribas@solcre.com>
2020-02-03 07:35:06 -0300

    refactor(Page): set session to navigate the site in admin mode

--- module/Columnis/src/Columnis/Controller/PageController.php
+++ module/Columnis/src/Columnis/Controller/PageController.php
@@ -2,34 +2,42 @@
 
 namespace Columnis\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
-use Zend\View\Model\JsonModel;
+use Columnis\Exception\Page\PageWithoutTemplateException;
 use Columnis\Model\Page;
 use Columnis\Model\Template;
 use Columnis\Utils\Directory;
-use Columnis\Exception\Page\PageWithoutTemplateException;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\JsonModel;
+use Zend\View\Model\ViewModel;
 
-class PageController extends AbstractActionController {
-
-    public function indexAction() {
-        $pageId = (int) $this->params()->fromRoute('pageId');
+class PageController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $pageId = (int)$this->params()->fromRoute('pageId');
         $lang = $this->params()->fromRoute('lang');
         $queryParams = $this->params()->fromQuery();
-        if(!is_array($queryParams)) {
-            $queryParams = array();
+        if (! is_array($queryParams)) {
+            $queryParams = [];
         }
+
+        if ($this->isAdminSessionSet() && $this->mustDestroyAdminSession($queryParams)) {
+            $this->destroyAdminSession();
+        } elseif (isset($queryParams['token']) && ! empty($queryParams['token'])) {
+            $this->setAdminSession($queryParams);
+        }
+
         $queryParams['lang'] = $lang;
         $page = $this->fetchPage($pageId, $queryParams, true);
 
-        if($page instanceof Page) {
+        if ($page instanceof Page) {
             $viewVariables = $page->getData();
             $template = $page->getTemplate();
-            
-            if((bool)$queryParams['debug']){
+
+            if ((bool)$queryParams['debug']) {
                 return new JsonModel($viewVariables);
             }
-            if($template->isValid()) {
+            if ($template->isValid()) {
                 $this->setPageAssets($template, $viewVariables);
 
                 $view = new ViewModel();
@@ -42,22 +50,58 @@ class PageController extends AbstractActionController {
         $this->getResponse()->setStatusCode(404);
     }
 
-    protected function fetchPage($pageId, Array $params = null, $withBreakpoints = false) {
-        $accessToken = $this->getRequest()->getHeaders()->get('Cookie')->columnis_token;
+    private function isAdminSessionSet()
+    {
+        session_start();
+        return isset($_SESSION['token']);
+    }
+
+    private function mustDestroyAdminSession($queryParams)
+    {
+        return isset($queryParams['logoutAdmin']) && ! empty($queryParams['logoutAdmin']);
+    }
+
+    private function destroyAdminSession()
+    {
+        session_start();
+        if ($this->isAdminSessionSet()) {
+            \session_unset($_SESSION['token']);
+        }
+    }
+
+    private function setAdminSession(array $queryParams)
+    {
+        session_start();
+        $_SESSION['token'] = $queryParams['token'];
+    }
+
+    protected function fetchPage($pageId, Array $params = null, $withBreakpoints = false)
+    {
+        $accessToken = $this->getAdminSession();
+        if ($accessToken === null) {
+            $accessToken = $this->getRequest()->getHeaders()->get('Cookie')->columnis_token;
+        }
         $page = new Page();
         $page->setId($pageId);
         $pageService = $this->getPageService($withBreakpoints);
         try {
-            if(!$pageService->fetch($page, $params, $accessToken)) {
+            if (! $pageService->fetch($page, $params, $accessToken)) {
                 return null;
             }
-        } catch(PageWithoutTemplateException $e) {
-            
+        } catch (PageWithoutTemplateException $e) {
+
         }
         return $page;
     }
 
-    public function getPageService($withBreakpoints = false) {
+    private function getAdminSession()
+    {
+        session_start();
+        return $_SESSION['token'];
+    }
+
+    public function getPageService($withBreakpoints = false)
+    {
         $serviceManager = $this->getServiceLocator();
         $pageService = $serviceManager->get('Columnis\Service\PageService');
         /* @var $pageService \Columnis\Service\PageService */
@@ -65,82 +109,94 @@ class PageController extends AbstractActionController {
         return $pageService;
     }
 
-    private function setPageAssets(Template $template, &$pageData) {
+    private function setPageAssets(Template $template, &$pageData)
+    {
         $excludes = $this->getExcludes();
         $jsAssets = $template->getAssets('js', $excludes);
         $cssAssets = $template->getAssets('css', $excludes);
 
-        $fiexedJsAssets = array();
-        $fixedCssAssets = array();
-        
+        $fiexedJsAssets = [];
+        $fixedCssAssets = [];
+
         $paths = $this->getAssetsPath();
-        
-        if(count($paths)) {
-            foreach($paths as $path) {
-                if(strpos($path, 'css') > -1) {
+
+        if (count($paths)) {
+            foreach ($paths as $path) {
+                if (strpos($path, 'css') > -1) {
                     $fixedCssAssets = array_merge($fixedCssAssets, $this->searchAssets($path, 'css', $excludes));
                     sort($fixedCssAssets);
-                }
-                else if(strpos($path, 'js') > -1) {
-                    $fiexedJsAssets = array_merge($fiexedJsAssets, $this->searchAssets($path, 'js', $excludes));
-                    sort($fiexedJsAssets);
+                } else {
+                    if (strpos($path, 'js') > -1) {
+                        $fiexedJsAssets = array_merge($fiexedJsAssets, $this->searchAssets($path, 'js', $excludes));
+                        sort($fiexedJsAssets);
+                    }
                 }
             }
         }
-        
+
         //Merge fixed with templates
         $jsAssets = array_merge($fiexedJsAssets, $jsAssets);
         $cssAssets = array_merge($fixedCssAssets, $cssAssets);
-        
-        if(is_array($pageData) && $pageData['page']) {
+
+        if (is_array($pageData) && $pageData['page']) {
             $pageData['page']['scripts'] = $this->setPublicRelativePath($jsAssets);
             $pageData['page']['stylesheets'] = $this->setPublicRelativePath($cssAssets);
         }
         return '';
     }
-    private function setPublicRelativePath(Array $assets = null) {
-        $ret = array();
+
+    private function getExcludes()
+    {
+        $ret = [];
+        $config = $this->getServiceLocator()->get('Config');
+        if (is_array($config) && isset($config['template_assets_resolver'])) {
+            $ret = $config['template_assets_resolver']['search_exclude'];
+        }
+        return $ret;
+    }
+
+    private function getAssetsPath()
+    {
+        $config = $this->getServiceLocator()->get('Config');
+        if (is_array($config)
+            && isset($config['asset_manager'])
+            && is_array($config['asset_manager']['resolver_configs'])
+            && isset($config['asset_manager']['resolver_configs']['paths'])
+        ) {
+            return $config['asset_manager']['resolver_configs']['paths'];
+        }
+        return [];
+    }
+
+    private function searchAssets($path, $extension, Array $excludes = null)
+    {
+        $assets = [];
+        $assetPath = realpath($path . DIRECTORY_SEPARATOR . 'fixed');
+        if (is_dir($assetPath)) {
+            $assets = Directory::recursiveSearchByExtension($assetPath, $extension, $excludes);
+        }
+        return $assets;
+    }
+
+    private function setPublicRelativePath(Array $assets = null)
+    {
+        $ret = [];
         if (count($assets) > 0) {
             $publicPath = realpath($this->getPublicPath()) . DIRECTORY_SEPARATOR;
-            foreach($assets as $asset) {
+            foreach ($assets as $asset) {
                 $ret[] = str_replace($publicPath, '', $asset);
             }
         }
         return $ret;
     }
-    private function getPublicPath() {
-        $ret = array();
+
+    private function getPublicPath()
+    {
+        $ret = [];
         $config = $this->getServiceLocator()->get('Config');
-        if(is_array($config) && isset($config['template_assets_resolver'])) {
+        if (is_array($config) && isset($config['template_assets_resolver'])) {
             $ret = $config['template_assets_resolver']['public_path'];
         }
         return $ret;
-    }
-    private function getExcludes() {
-        $ret = array();
-        $config = $this->getServiceLocator()->get('Config');
-        if(is_array($config) && isset($config['template_assets_resolver'])) {
-            $ret = $config['template_assets_resolver']['search_exclude'];
-        }
-        return $ret;
-    }
-    private function getAssetsPath() {
-        $config = $this->getServiceLocator()->get('Config');
-        if(is_array($config) &&
-                isset($config['asset_manager']) &&
-                is_array($config['asset_manager']['resolver_configs']) &&
-                isset($config['asset_manager']['resolver_configs']['paths'])) {
-            return $config['asset_manager']['resolver_configs']['paths'];
-        }
-        return array();
-    }
-
-    private function searchAssets($path, $extension, Array $excludes = null) {
-        $assets = array();
-        $assetPath = realpath($path.DIRECTORY_SEPARATOR.'fixed');
-        if (is_dir($assetPath)) {
-            $assets = Directory::recursiveSearchByExtension($assetPath, $extension, $excludes);
-        }
-        return $assets;
     }
 }
